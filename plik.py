2b5449ea617f18dee520a2c35a46b29aadb06181
jan16210 <jan.ras11111@gmail.com>
2026-01-12 17:41:10 +0000

    dlugosc hasla

--- plik.py
+++ plik.py
@@ -1,2 +1,14 @@
-print("Test")
-print("test2")
+import random 
+zmienna = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+ans = int(input("Podaj dlugosc hasla: "))
+
+haslo = " "
+for i in range(ans):
+    randomSign = random.choice(zmienna)
+    haslo += randomSign
+
+
+
+
+print(haslo)
+
