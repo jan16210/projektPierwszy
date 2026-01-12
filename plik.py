cc3ab7963fa85b6ca4f93f1b9849a986ca2027d4
jan16210 <jan.ras11111@gmail.com>
2026-01-12 17:56:38 +0000

    zadanie drugie

--- plik.py
+++ plik.py
@@ -1,14 +1,4 @@
-import random 
-zmienna = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-ans = int(input("Podaj dlugosc hasla: "))
-
-haslo = " "
-for i in range(ans):
-    randomSign = random.choice(zmienna)
-    haslo += randomSign
-
-
-
-
-print(haslo)
-
+ans = input("Wpisz swoje imie")
+print("*******")
+print("*",ans,"*")
+print("*******")
